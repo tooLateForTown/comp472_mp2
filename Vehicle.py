53cb0373fb76000b238acf13175b3bd3f262503f
Derek James <derek2344@protonmail.com>
2022-11-15 18:58:53 -0500

    basic game loader

---
+++ Vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    letter = '?'
+    x = 0
+    y = 0
+    length = 0
+    escaped = False
+    gas = 100
+
+    def __init__(self, letter):
+        self.letter = letter
+
+    def __str__(self):
+        return f"Vehicle {self.letter}"
--- main.py
+++ main.py
@@ -1,1 +1,66 @@
-print ("Hello mp2")
+from array import *
+from Vehicle import Vehicle
+import numpy as np
+
+board = np.full((6,6), '.')
+vehicles = []
+
+def main():
+    print("MP2: Rush-Hour")
+    valid = False
+
+    while not valid:
+        setup = input("Enter your game setup as a string: ")
+        valid = load_game(setup)
+    print("Valid board loaded")
+    show_board()
+
+
+def show_board():
+    print('   a b c d e f')
+    print("  -------------")
+    count = 1
+    for row in board:
+        print(count, end='| ')
+        count += 1
+        for col in row:
+            print(col, end=' ')
+        print()
+    print("  -------------")
+    print(f"[{len(vehicles)} vehicles]")
+
+
+
+def load_game(setup) -> bool:
+    setup = setup.strip()
+    if len(setup) < 36:
+        print("Setup needs to be at least 36 characters")
+        return False
+    # load Board array
+    for row in range(0,6):
+        for col in range(0,6):
+            board[row][col] = setup[row*6+col]
+    # identify the vehicles
+    for row in range(0,6):
+        for col in range(0,6):
+            letter = board[row][col]
+            if letter != '.':
+                vehicle = get_vehicle(letter)
+                if vehicle is None:
+                    v = Vehicle(letter)
+                    print(f"added {v}")
+                    vehicles.append(v)
+
+
+    print(board.shape)
+    return True
+
+def get_vehicle(letter):
+    for v in vehicles:
+        if v.letter == letter:
+            return v
+    return None
+
+
+if __name__ == "__main__":
+    main()
